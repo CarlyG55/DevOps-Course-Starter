d38ce8916aa52a31ddbfd0ffdc304002bf600fca
CarGil <carly.gilson@softwire.com>
2022-07-29 17:15:08 +0100

    DEVOPS-1 Display to do items and add functionality to add new items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,4 +8,11 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    to_do_items = get_items()
+    return render_template('index.html', to_do_items = to_do_items)
+
+
+@app.route('/submit', methods=["POST"])
+def submit_form():
+    add_item(request.form.get('title'))
+    return redirect('/')
